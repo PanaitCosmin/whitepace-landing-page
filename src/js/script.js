8e574489a19e6956db187dcba5d4a8068e33fda5
PanaitCosmin <cosmindaniel.panait@gmail.com>
2024-11-16 17:22:16 +0200

    DOM manipulation dropdown menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,9 @@
-console.log("Hello World")
+const navLinks = document.querySelectorAll('.navbar__link')
+const navDropdowns = document.querySelectorAll('.navbar__dropdown')
+
+navLinks.forEach((link, idx) => {
+
+    link.addEventListener('click', () => {
+        navDropdowns[idx].classList.toggle('navbar__dropdown--show')
+    })
+})
