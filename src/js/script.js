17d149d64d06fe1cf59ec680567b936ce9330746
PanaitCosmin <cosmindaniel.panait@gmail.com>
2024-11-24 15:05:36 +0200

    Pagination update payment cards

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+// Dropdown Menu
 const navLinks = document.querySelectorAll('.navbar__link')
 const navDropdowns = document.querySelectorAll('.navbar__dropdown')
 
@@ -7,3 +8,31 @@ navLinks.forEach((link, idx) => {
         navDropdowns[idx].classList.toggle('navbar__dropdown--show')
     })
 })
+
+// Payment Pagination
+let currentCardIdx = 0;
+const cards = document.querySelectorAll('.card');
+const dots = document.querySelectorAll('.payment__dot');
+
+function updateCard() {
+    // Selected card visible
+    cards.forEach(card => card.classList.remove('card--active'))
+    cards[currentCardIdx].classList.add('card--active')
+
+    // Selected dot active
+    dots.forEach(dot => dot.classList.remove('payment__dot--active'))
+    dots[currentCardIdx].classList.add('payment__dot--active')
+}
+
+dots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        currentCardIdx = index;
+        updateCard();
+    })
+})
+
+// Initialize the first card
+updateCard();
+
+// Re-run updateReview on window resize to ensure proper layout on viewport change
+window.addEventListener("resize", updateCard);
